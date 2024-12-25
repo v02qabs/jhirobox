112904ede886f7bb28660fbcdfd1f864c62a1bcb
v02qabs <takesue090@gmail.com>
2024-12-25 21:57:36 +0900

    ipadd.java

---
+++ ipAdd.java
@@ -0,0 +1,13 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ipAdd{
+    public static void main(String[] args) {
+        try {
+            InetAddress address = InetAddress.getByName("user-PC-LS150RSR");
+            System.out.println("www.example.com's IP address: " + address.getHostAddress()); 
+        } catch (UnknownHostException ex) {
+            System.out.println("IP address could not be resolved: " + ex.getMessage());
+        }
+    }
+}
