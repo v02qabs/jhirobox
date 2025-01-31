a27927fd36780f535883a5e297d785726cae4291
v0qabs <takesue090@gmail.com>
2025-01-31 09:41:02 +0000

    ok

--- end.java
+++ end.java
@@ -5,22 +5,22 @@ import java.util.stream.Collectors;
 
 public class end{
     public static void main(String[] args) {
-        String inputFile = args[0];  // 入力ファイル
-        String outputFile = args[1]; // 出力ファイル
+        String inputFile = args[0]; 
+	String outputFile = args[1]; 
 
         try {
-            // ファイルを読み込む
             List<String> lines = Files.readAllLines(Paths.get(inputFile));
 
-            // 各行の先頭と末尾に文字を追加
             List<String> modifiedLines = lines.stream()
-                .map(line -> args[2] + line + " " + line + " " + args[3] + ".mp3")
+                .map(line -> args[2] + " -i " + line + line +  args[3])
                 .collect(Collectors.toList());
 
-            // 結果をファイルに書き込む
-            Files.write(Paths.get(outputFile), modifiedLines);
 
-            System.out.println("処理が完了しました。");
+
+		Files.write(Paths.get(outputFile), modifiedLines);
+	
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
